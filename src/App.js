23041daeb46cf17c17fccbce3b11499fc7a72cab
shashipreetham23 <131871465+shashipreetham23@users.noreply.github.com>
2023-07-21 15:56:39 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/" component={Home} />
@@ -16,7 +15,6 @@ const App = () => (
       <Route exact path="/cart" component={Cart} />
       <Route component={NotFound} />
     </Switch>
-  </BrowserRouter>
 )
 
 export default App
